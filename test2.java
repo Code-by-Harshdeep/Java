01833fb246b6429e8404e4a26ba2f756863f86cf
Harsh <hs855787@gmail.com>
2026-07-13 21:35:32 +0530

    test

--- test2.java
+++ test2.java
@@ -2,6 +2,9 @@ public class test2 {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        String name = "Harsh";
+        int age = 22;
+        System.out.println(name);
     }
-    
+
 }
