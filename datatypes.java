8dca08f1ffe1406f3bb8ee71ba07dad3e9585434
Harsh <hs855787@gmail.com>
2026-07-13 22:24:06 +0530

    string methods

---
+++ datatypes.java
@@ -0,0 +1,30 @@
+public class datatypes {
+
+    public static void main(String[] args){
+
+       //Primitive Data Types in Java
+       //int
+       //float
+       //double
+       //char
+       //boolean
+       //byte 
+       byte age =20;
+       int phone=1234567890;
+       long phone2=12345678900L;
+       float pi =3.14F;
+       char letter ='@';
+       boolean isAdult=false;
+     
+       //Non-Primitve Types
+       String name="Harsh";
+       String name2=new String("Alice");
+       String name3=name+" and "+name2;
+       System.out.println(name.length()); 
+       System.out.println(name2);
+       System.out.println(name3);
+       System.out.println(name.charAt(0));
+    
+    }
+    
+}
