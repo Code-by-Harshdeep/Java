866938c6b6a94c211252659914ba7cef492bb47c
Harsh <hs855787@gmail.com>
2026-07-28 20:41:06 +0530

    series done 1,-2,3,-4....n

---
+++ series.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class series {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=s.nextInt();
+        int sum=0;
+        for (int i = 0; i <= n; i++) {
+            if(i%2==0){
+                sum=sum-i;
+            }
+            else{
+                sum=sum+i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
